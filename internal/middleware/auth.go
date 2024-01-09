bccde3f672b805951f7dd08388c865ee20727032
aswinbennyofficial <aswinbenny.official@gmail.com>
2024-01-09 17:03:21 +0530

    implemented token refresh on authorize middleware

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -5,6 +5,7 @@ import(
 	"log"
 	"context"
 	"github.com/aswinbennyofficial/jwt-auth-golang/internal/utility"
+	"time"
 )
 
 
@@ -18,6 +19,32 @@ func LoginRequired(next http.Handler) http.Handler {
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
+
+
+		// Check if the token is nearing expiration (e.g., within 2 minutes)
+		expirationThreshold := time.Now().Add(2 * time.Minute)
+		if claims.ExpiresAt.Time.Before(expirationThreshold) {
+			// Token is nearing expiration, generate a new token
+			signedToken, err := utility.GenerateToken(claims.Username)
+			if err != nil {
+				log.Println("ERROR OCCURRED WHILE CREATING JWT TOKEN: ", err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			// Setting JWT claims for the new token
+			expirationTime := time.Now().Add(5 * time.Minute)
+
+			http.SetCookie(w, &http.Cookie{
+				Name:    "JWtoken",
+				Value:   signedToken,
+				Expires: expirationTime,
+			})
+
+			log.Println("TOKEN REFRESH SUCCESSFUL")
+		}
+
+
 
 		// Add claims to the request context for use in subsequent handlers
 		ctx := r.Context()
